3061a8110cd6b4df42fe0c09fa0791573345b895
3yassu <68251430+3yassu@users.noreply.github.com>
2025-04-11 13:20:06 -0500

    Add user_input support

--- main.cpp
+++ main.cpp
@@ -24,20 +24,68 @@ pair<vector<vector<T>>, vector<vector<T>>> readMatrixFromFile(int N, ifstream &f
 }
 template <typename T>
 void run(pair<vector<vector<T>>, vector<vector<T>>> matrix){
-        Matrix<T> bot(matrix.first);
-        Matrix<T> tob(matrix.second);
-	bot.print_matrix();
-	tob.print_matrix();
-        bot+tob;
-        bot*tob;
-        cout << "major Diagonal: ";
-        bot.sum_diagonal_major();
-        cout << "minor diagonal: ";
-        bot.sum_diagonal_minor();
-        bot.swap_cols(1, 2);
-        bot.swap_rows(1, 2);
-        bot.set_value(1, 1, 0);
- 
+    Matrix<T> bot(matrix.first);
+    Matrix<T> tob(matrix.second);
+    bot+tob;
+    bot*tob;
+    cout << "major Diagonal: ";
+    bot.sum_diagonal_major();
+    cout << "minor diagonal: ";
+    bot.sum_diagonal_minor();
+    int matrix_check;
+    while(true){
+        cout << "which matrix do you want to manipulate for the rest of the program (1 or 2): ";
+        cin >> matrix_check;
+        if(!(matrix_check <= 2 && matrix_check >= 1)){cout << "Not a valid number, try again (1 or 2)" << endl;}
+        else{break;}
+    }
+    while(true) {
+        int col_one; int col_two; 
+        cout << endl << "enter column 1 (starting at 0): ";
+        cin >> col_one;
+        cout << endl << "enter column 2 (starting at 0): ";
+        cin >> col_two; cout << endl;
+        int size = matrix_check == 1? bot.get_size() : tob.get_size();
+        if(!(col_one >= 0 && col_two >= 0 && col_one < size && col_two < size)){cout << "Not a valid number, columns has to be greater than 0 and less than " << size << endl; continue;}
+        if(matrix_check == 1){
+            bot.swap_cols(col_one, col_two);
+        }else{
+            tob.swap_cols(col_one, col_two);
+        }
+        break;
+    }
+    while(true) {
+        int row_one; int row_two; 
+        cout << endl << "enter row 1 (starting at 0): ";
+        cin >> row_one;
+        cout << endl << "enter row 2 (starting at 0): ";
+        cin >> row_two; cout << endl;
+        int size = matrix_check == 1? bot.get_size() : tob.get_size();
+        if(!(row_one >= 0 && row_two >= 0 && row_one < size && row_two < size)){cout << "Not a valid number, rows has to be greater than 0 and less than " << size << endl; continue;}
+        if(matrix_check == 1){
+            bot.swap_rows(row_one, row_two);
+        }else{
+            tob.swap_rows(row_one, row_two);
+        }
+        break;
+    }
+    while(true) {
+        int pos_one; int pos_two; T value;
+        cout << endl << "enter position i (starting at 0): ";
+        cin >> pos_one;
+        cout << endl << "enter position j (starting at 0): ";
+        cin >> pos_two;
+        cout << endl << "Enter value: ";
+        cin >> value; cout << endl;
+        int size = matrix_check == 1? bot.get_size() : tob.get_size();
+        if(!(pos_one >= 0 && pos_two >= 0 && pos_one < size && pos_two < size)){cout << "Not a valid number, positions has to be greater than 0 and less than " << size << endl; continue;}
+        if(matrix_check == 1){
+            bot.set_value(pos_two, pos_one, value);
+        }else{
+            tob.set_value(pos_two, pos_one, value);
+        }
+        break;
+    }
 }
 int main() {
     cout  << "File name please: ";
@@ -58,4 +106,3 @@ int main() {
     }
     return 0;
 }
-
